35dd47a87da1968fc0bc1c92f402dc49bf5accd7
Dan Visintainer <dan@danvisintainer.com>
2016-05-28 23:37:42 -0400

    update photo ratio grabber to add indexes and dims

--- _plugins/add_img_ratio.rb
+++ _plugins/add_img_ratio.rb
@@ -16,21 +16,26 @@ Jekyll::Hooks.register :posts, :post_render do |post|
                 end
             end
 
-            image_tags.each do |img|
-                unless img.attr('src').end_with? ".svg"
+            image_tags.each_with_index do |img, i|
+                if img.attr('src') && img.attr('data-src').nil? && !img.attr('src').end_with?(".svg")
                     src_digest = Digest::MD5.hexdigest(img.attr('src'))
 
                     unless hash[src_digest]
                         begin
                             hash[src_digest] = {}
                             hash[src_digest]['size'] = FastImage.size(img.attr('src'))
-                            hash[src_digest]['ratio'] = hash[src_digest]['size'][0].to_f / hash[src_digest]['size'][1].to_f
+                            hash[src_digest]['data-ratio'] = hash[src_digest]['size'][0].to_f / hash[src_digest]['size'][1].to_f
                         rescue Exception => e
                             puts "Couldn't fetch size for image " << img.attr('src') << ", due to error: " << e
                         end
                     end
 
-                    img['ratio'] = hash[src_digest]['ratio']
+                    img['data-ratio'] = hash[src_digest]['data-ratio']
+                    img['data-src'] = img.attr('src')
+                    img['data-width'] = hash[src_digest]['size'][0]
+                    img['data-height'] = hash[src_digest]['size'][1]
+                    img['data-index'] = i
+                    img['src'] = ''
                 end
             end
 
